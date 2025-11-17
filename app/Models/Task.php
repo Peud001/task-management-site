a58a80ce1bcf0aa1de37f21dcc33769aab585aec
Solomon Udofia <meetsolomonudofia@gmail.com>
2025-11-17 15:05:13 +0100

    implements fillable and relationship in the Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'priority',
+        'projectid'
+    ];
+
+    public function projects(): BelongsTo{
+        return $this->belongsTo(Project::class);
+    }
 }
