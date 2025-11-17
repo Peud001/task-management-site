03699a193c0ade71710220359de9e1801e9994a9
Solomon Udofia <meetsolomonudofia@gmail.com>
2025-11-17 18:34:53 +0100

    setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('task.index');
 });
+
+Route::resource('tasks', TaskController::class)->except(['show']);
+Route::post('tasks/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
+
+Route::resource('project', ProjectController::class)->only(['index', 'store']);
